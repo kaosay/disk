eb5eaf446365036e68d5ac9d63d06d97f2b71412
kaosay <547091630@qq.com>
2021-10-13 22:35:32 +0800

    add all

---
+++ logic_disk_data.sh
@@ -0,0 +1,14 @@
+
+pvcreate /dev/vdb
+
+vgcreate vg_log /dev/vdb
+
+lvcreate -l 100%free vg_log -n lv_log
+
+mkdir /data
+
+mkfs.ext4 /dev/mapper/vg_log-lv_log
+
+mount /dev/mapper/vg_log-lv_log /data/
+
+sed -i '$a /dev/mapper/vg_log-lv_log  /data  ext4  defaults  0  0' /etc/fstab
